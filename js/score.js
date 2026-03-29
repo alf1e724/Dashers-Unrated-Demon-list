2546e9a89b77095ff5225d403afc779202bb9270
smegful dashers <xmollyxgood@gmail.com>
2026-03-29 16:32:02 +0100

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (rank <= 100) ? 350 * Math.pow(0.925, rank-1) : 0;
+    let score = (rank <= 100) ? 350 * Math.pow(0.9, rank-1) : 0;
 
     score = Math.max(0, score);
 
